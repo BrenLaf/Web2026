6db08dafc972598ecc3de74458d410b01960a96b
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-02 18:17:53 +0000

    Mon, Mar 2, 2026, 10:17 AM -08:00

--- script/expanding.js
+++ script/expanding.js
@@ -2,6 +2,13 @@ pannels = document.querySelectorAll('.pannel');
 
 for (let i = 0; i < 5; i++){
     pannels[i].addEventListener('click', () => {
+        removeActiveFromAllPannels()
         pannels[i].classList.add('active')
     })
+}
+
+function removeActiveFromAllPannels() {
+    for(let i = 0; 1 < 5; i++){
+        pannels[i].classList.remove('active')
+    }
 }
