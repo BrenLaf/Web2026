3d76623c89e0f88117f5e1f3dcd0cce060b82193
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-02 18:19:01 +0000

    Mon, Mar 2, 2026, 10:19 AM -08:00

--- script/expanding.js
+++ script/expanding.js
@@ -8,7 +8,7 @@ for (let i = 0; i < 5; i++){
 }
 
 function removeActiveFromAllPannels() {
-    for(let i = 0; 1 < 5; i++){
+    for(let i = 0; i < 5; i++){
         pannels[i].classList.remove('active')
     }
 }
