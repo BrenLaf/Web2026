7b3ca867be260151c562858ce6039ded92d8075f
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-02 18:13:56 +0000

    Mon, Mar 2, 2026, 10:13 AM -08:00

--- script/expanding.js
+++ script/expanding.js
@@ -1,6 +1,6 @@
 pannels = document.querySelectorAll('.pannel');
 
-for (int i = 0; i < 5; i++){
+for (let i = 0; i < 5; i++){
     pannels[i].addEventListener('click', () => {
         pannels[i].classList.add('active')
     })
