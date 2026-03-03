514181c629aca02f3205e503a93eae36791c71f5
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-03 18:25:46 +0000

    Tue, Mar 3, 2026, 10:25 AM -08:00

--- script/event.js
+++ script/event.js
@@ -1,13 +1,14 @@
 clickBox = document.querySelector('.click')
 
-colors= ['blue', 'green', 'purple', 'black', 'red']
+colors = ['blue', 'green', 'purple', 'black', 'rgb(255, 105, 180)','red']
+textColor = ['white', 'red', 'black', 'white', 'white', 'black']
 index = 0;
 
 clickBox.addEventListener('click', () => {
     clickBox.style.background = colors[index]
+    clickBox.style.color = textColor[index]
     index++
     if (index == 6) {
         index = 0
     }
-    clickBox.style.color = 'white'
 })
