100171e6b969c422404ef8e6473156b2d2e5d8cf
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-03 18:22:34 +0000

    Tue, Mar 3, 2026, 10:22 AM -08:00

--- script/event.js
+++ script/event.js
@@ -1,7 +1,13 @@
 clickBox = document.querySelector('.click')
 
-clickBox.addEventListener('click',() => {
-    clickBox.style.background = 'blue'
+colors= ['blue', 'green', 'purple', 'black', 'red']
+index = 0;
+
+clickBox.addEventListener('click', () => {
+    clickBox.style.background = colors[index]
+    index++
+    if (index == 6) {
+        index = 0
+    }
     clickBox.style.color = 'white'
 })
-
