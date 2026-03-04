7b7de317066da0da24dd9a72c31353de0082254a
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-04 17:54:05 +0000

    Wed, Mar 4, 2026, 9:54 AM -08:00

--- script/event.js
+++ script/event.js
@@ -1,4 +1,8 @@
 clickBox = document.querySelector('.click')
+spinBox = document.querySelector('.spin')
+moveBox = document.querySelector('.move')
+hoverBox = document.querySelector('.hover')
+dblclickBox = document.querySelector('.dblclick')
 
 colors = ['blue', 'green', 'purple', 'black', 'rgb(255, 105, 180)','red']
 textColor = ['white', 'red', 'black', 'white', 'white', 'black']
@@ -11,4 +15,34 @@ clickBox.addEventListener('click', () => {
     if (index == colors.length) {
         index = 0
     }
+})
+
+x = 0
+y = 0
+
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'ArrowRight') {x += 10}
+    if (event.key == 'ArrowUp') {y -= 10}
+    if (event.key == 'ArrowDown') {y += 10}
+    if (event.key == 'ArrowLeft') {x -= 10}
+    moveBox.style.transform = `translate(${x}px, ${y}px)`
+})
+
+h = 100
+w = 300
+
+hoverBox.addEventListener('mouseenter', () => {
+    hoverBox.style.transform = "scale(1.3)"
+})
+
+hoverBox.addEventListener('mouseleave', (event) => {
+    hoverBox.style.transform = "scale(1)"
+})
+
+
+dblclickBox.addEventListener("dblclick", () => {
+    dblclickBox.style.background = colors[index]
+    dblclickBox.style.color = textColor[index]
+    index ++
+    if (index == colors.length) {index = 0}
 })
