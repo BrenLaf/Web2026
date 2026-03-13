12912b45dca7f7990cf5c09b9e727829265a13bd
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-13 16:24:32 +0000

    Fri, Mar 13, 2026, 9:24 AM -07:00

--- script/loading.js
+++ script/loading.js
@@ -1,20 +1,19 @@
-loadText = document.querySelector('.loading-text')
-bg document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
 
-load = 0
+let load = 0
 
-int = setInterval(bluring, 30)
+const int = setInterval(bluring, 30)
 
 function bluring() {
     load = load + 1
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = 1-load/100
-    bg.style.filter = `blur(50-${load/2}px)`
 
-    if(load > 99) {
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1 - load / 100
+
+    bg.style.filter = `blur(${50 - load / 2}px)`
+
+    if (load > 99) {
         clearInterval(int)
     }
 }
-
-
-
--- script/sound.js
+++ script/sound.js
@@ -7,9 +7,18 @@ for (let i = 0; i < 6; i++) {
     btn.innerText = sounds[i]
     btn.classList.add('btn')
     btn.addEventListener('click', () => {
+        stopSongs()
         document.querySelector(`.${sounds[i]}`).play()
     })
     buttons.appendChild(btn)
+}
+
+function stopSongs() {
+    for (let i = 0; i < sounds.length; i++)
+    {
+        document.querySelector(`.${sounds[i]}`).pause()
+        document.querySelector(`.${sounds[i]}`).currentTime = 0
+    }
 }
 
 
