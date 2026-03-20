50362e86c705bbf5f02c64421884fa857ecf7af1
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-20 16:20:49 +0000

    Fri, Mar 20, 2026, 9:20 AM -07:00

--- script/insects.js
+++ script/insects.js
@@ -1,6 +1,7 @@
 screens = document.querySelectorAll('.screen');
 choose_insect_btn = document.querySelectorAll('.choose-insect-btn');
 start_btn = document.getElementById('start-btn');
+game_container = document.querySelector('.game-container')
 
 start_btn.addEventListener('click', () => {
     screens[0].classList.add('up');
@@ -10,12 +11,22 @@ start_btn.addEventListener('click', () => {
 for (let i = 0; i < choose_insect_btn.length; i++)
 {
     choose_insect_btn[i].addEventListener('click', () => {
-    screens[1].classList.add('up')
-    img = choose_insect_btn[i].querySelector('img')
-    src = img.getAttribute('src')
-    alt = img.getAttribute('alt')
-    console.log(src)
+    screens[1].classList.add('up');
+    img = choose_insect_btn[i].querySelector('img');
+    src = img.getAttribute('src');
+    alt = img.getAttribute('alt');
+    startGame();
     })
 }
 
+function startGame() {
+    // TO DO
+    setTimeout(createInsect, 1000);
+}
 
+function createInsect() {
+    insect = document.createElement('div');
+    insect.classList.add('insect');
+    insect.innerHtml = `<img src="images/moth.png" alt="">`;
+    game_container.appendChild(insect)
+}
