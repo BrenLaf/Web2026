5f84c36789df31ab18c08a6458e155197a1934d4
BrenLaf <LaFranceBrendan@gmail.com>
2026-03-25 10:06:45 -0700

    update website

--- script/homepage.js
+++ script/homepage.js
@@ -1,11 +1,14 @@
 dropdown_content = document.querySelector('.dropdown-content');
 dropdown = document.querySelector('.dropdown');
 dropbtn = document.querySelector('.dropbtn');
+arrow = document.querySelector('.fa-solid.fa-angle-down');
 
 dropbtn.addEventListener('click', () => {
     if (dropdown_content.classList.contains('show')) {
         dropdown_content.classList.remove('show')
+        arrow.classList.remove('up');
     } else {
+        arrow.classList.add('up');
         dropdown_content.classList.add('show')
     }
 })
